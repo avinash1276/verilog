6bf1f1717023967112652d86a2dc4b5530dd72ed
avinash1276 <avinashjeevanandam12@gmail.com>
2026-07-16 23:41:27 +0530

    Create tb161.v

---
+++ mux/tb161.v
@@ -0,0 +1,18 @@
+module mux161_tb();
+reg [15:0]a;
+reg [3:0]s;
+wire y;
+mux161 dut(.a(a),.s(s),.y(y));
+integer i;
+initial begin
+    $dumpfile("mux161.vcd");
+    $dumpvars(0,dut);
+    $monitor("time=%0t,a=%b,s=%b,y=%b",$time,a,s,y);
+    for(i=0;i<1048576;i=i+1) begin
+    {a,s}=10756;
+    #5;
+    end
+    $finish;
+end
+endmodule
+
