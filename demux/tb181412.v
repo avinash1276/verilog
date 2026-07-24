a553720e40e319211a20bac18be0cfcb07f6baa8
avinash1276 <avinashjeevanandam12@gmail.com>
2026-07-24 17:10:16 +0530

    Create tb181412.v

---
+++ demux/tb181412.v
@@ -0,0 +1,20 @@
+module demux181412_tb();
+reg x;
+reg [2:0]s;
+wire [7:0]y;
+demux181412 dut(.x(x),.s(s),.y(y));
+integer i,j;
+initial begin
+    $dumpfile("demux181412.vcd");
+    $dumpvars(0,dut);
+    $monitor("time=%0t,x=%b,s=%b,y=%b",$time,x,s,y);
+    for (j=0;j<2;j=j+1)begin
+        x=j;
+    for (i=0;i<8;i=i+1) begin
+        s=i;
+        #5;
+    end
+    end
+    $finish;
+end
+endmodule
