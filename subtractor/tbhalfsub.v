9b7475bd0b4e2a7cd9d6ca478738f106cf0cc034
avinash1276 <avinashjeevanandam12@gmail.com>
2026-07-24 18:35:01 +0530

    Add testbench for half subtractor module

---
+++ subtractor/tbhalfsub.v
@@ -0,0 +1,16 @@
+module subtractor_tb();
+reg a,b;
+wire difference,borrow;
+subtractor dut(.a(a),.b(b),.difference(difference),.borrow(borrow));
+integer i;
+initial begin
+    $dumpfile("subtractor.vcd");
+    $dumpvars(0,dut);
+    $monitor("time=%0t,%b,b=%b,difference=%b,borrow=%b",$time,a,b,difference,borrow);
+    for(i=0;i<4;i=i+1) begin
+    {a,b}= i;
+    #5;
+    end
+    $finish;
+end
+endmodule
