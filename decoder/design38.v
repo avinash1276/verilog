2c74606d23790e7562841a5ef317bdab756a471d
avinash1276 <avinashjeevanandam12@gmail.com>
2026-07-25 14:49:49 +0530

    Create design38.v

---
+++ decoder/design38.v
@@ -0,0 +1,18 @@
+module decoder38(
+    input a,b,c,
+    output reg [7:0]y
+);
+always @(*) begin
+    y=8'b00000000;
+    case({a,b,c})
+    3'b000: y[0]=1'b1;
+    3'b001: y[1]=1'b1;
+    3'b010: y[2]=1'b1;
+    3'b011: y[3]=1'b1;
+    3'b100: y[4]=1'b1;
+    3'b101: y[5]=1'b1;
+    3'b110: y[6]=1'b1;
+    3'b111: y[7]=1'b1;
+    endcase
+end
+endmodule
