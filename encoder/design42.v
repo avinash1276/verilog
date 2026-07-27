dbbd7e5731a4bb44f3fc6e80547f814b441acf7f
avinash1276 <avinashjeevanandam12@gmail.com>
2026-07-27 22:44:09 +0530

    Create design42.v

---
+++ encoder/design42.v
@@ -0,0 +1,14 @@
+module encoder42(
+    input [3:0]x,
+    output reg [1:0]y
+);
+always @(*) begin
+    y=2'b00;
+    case(x)
+    4'b0001: y=2'b00;
+    4'b0010: y=2'b01;
+    4'b0100: y=2'b10;
+    4'b1000: y=2'b11;
+    endcase
+end
+endmodule
