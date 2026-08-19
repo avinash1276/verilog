65da238c7166b5b1c5a89e632263850a8d405b21
avinash1276 <avinashjeevanandam12@gmail.com>
2026-08-19 14:39:21 +0530

    Rename test module for logical right shifter

--- SHIFTER/tblogicalrightshifter.v
+++ SHIFTER/tblogicalrightshifter.v
@@ -1,10 +1,10 @@
-module rightshifter4bit_tb();
+module logicalrightshifter4bit_tb();
   reg [3:0]a;
   wire [3:0]x;
-  rightshifter4bit dut(.a(a),.x(x));
+  logicalrightshifter4bit dut(.a(a),.x(x));
   integer i;
   initial begin
-    $dumpfile("rightshifter4bit.vcd");
+    $dumpfile("logicalrightshifter4bit.vcd");
     $dumpvars(0,dut);
     $monitor("a=%b,x=%b",a,x);
     for(i=0;i<16;i=i+1) begin
