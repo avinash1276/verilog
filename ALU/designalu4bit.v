0181f757c027c068826e44ae64418593981c0460
avinash1276 <avinashjeevanandam12@gmail.com>
2026-08-19 10:54:08 +0530

    Enhance ALU4bit with carry and borrow outputs
    Added carry and borrow outputs to the ALU module and updated the addition and subtraction logic to handle carry and borrow.

--- ALU/designalu4bit.v
+++ ALU/designalu4bit.v
@@ -1,15 +1,22 @@
 module ALU4bit(
     input [3:0] a, b,
     input [1:0] selectlines,
-    output reg [3:0] x
+  output reg [3:0] x,
+  output reg carry,
+  output reg borrow
 );
 
 always @(*) begin
+  carry = 0;
+  borrow = 0;
     case(selectlines)
         2'b00: x = a & b;   
         2'b01: x = a | b; 
-        2'b10: x = a + b;   
-        2'b11: x = a - b;  
+      2'b10: {carry,x} = a + b;   
+        2'b11: begin
+          x = a - b; 
+          borrow = (a<b);
+        end
     endcase
 end
 
