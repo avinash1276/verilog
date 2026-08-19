c71382e39e1684873a95a365a4319f67b3fc87e8
avinash1276 <avinashjeevanandam12@gmail.com>
2026-08-19 14:41:38 +0530

    Add testbench for rotate left shifter module

---
+++ SHIFTER/tbrotateleftckt.v
@@ -0,0 +1,16 @@
+module rotateleftshifter_tb();
+  reg [3:0]a;
+  wire [3:0]x;
+  rotateleftshifter dut(.a(a),.x(x));
+  integer i;
+  initial begin
+    $dumpfile("rotateleftshifter.vcd");
+    $dumpvars(0,dut);
+    $monitor("a=%b,x=%b",a,x);
+    for(i=0;i<16;i=i+1) begin
+      a = i;
+      #5;
+    end
+    $finish;
+  end
+                        endmodule
