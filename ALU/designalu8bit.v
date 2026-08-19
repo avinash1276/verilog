06bfe0fdd3abeb8107760d81fcdf73ad62db2226
avinash1276 <avinashjeevanandam12@gmail.com>
2026-08-19 10:40:06 +0530

    Update designalu8bit.v

--- ALU/designalu8bit.v
+++ ALU/designalu8bit.v
@@ -1,19 +1,26 @@
 module ALU8bit(
     input [7:0] a, b,
     input [2:0] s,
-    output reg [7:0] x
+  output reg [7:0] x,
+  output reg carry,
+  output reg borrow
 );
 
 always @(*) begin
+  carry = 0;
+  borrow = 0;
     case(s)
         3'd0: x = a & b;  // AND
         3'd1: x = a | b;  // OR
-        3'd2: x = a + b;  // ADDITION
-        3'd3: x = a - b;  // SUBTRACTION
+        3'd2: {carry,x} = a + b;  // ADDITION
+        3'd3: begin
+          x = a - b;  // SUBTRACTION
+          borrow = (a<b);
+        end
         3'd4: x = a ^ b;  // XOR
         3'd5: x = ~a;     // NOT A
-        3'd6: x = a + 1;  // INCREMENT A
-        3'd7: x = b + 1;  // INCREMENT B
+        3'd6: {carry,x} = a + 1;  // INCREMENT A
+        3'd7: {carry,x} = b + 1;  // INCREMENT B
     endcase
 end
 
