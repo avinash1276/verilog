b34333b866410a8b28b6dbfa1e24c92845987131
avinash1276 <avinashjeevanandam12@gmail.com>
2026-08-20 10:24:22 +0530

    Create tbbitreverser4bit.v

---
+++ Bit Reverser/tbbitreverser4bit.v
@@ -0,0 +1,16 @@
+module Bitreverser4bit_tb();
+  reg [3:0]a;
+  wire [3:0]x;
+  Bitreverser4bit dut(.a(a),.x(x));
+  integer i;
+  initial begin
+    $dumpfile("Bitreverser4bit.vcd");
+    $dumpvars(0,dut);
+    $monitor("a=%b,x=%b",a,x);
+    for(i=0;i<16;i=i+1) begin
+      a = i;
+      #5;
+    end
+    $finish;
+  end
+endmodule
