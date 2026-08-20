528db1958d76ae187974ff5e269ea695a5bd0bf0
avinash1276 <avinashjeevanandam12@gmail.com>
2026-08-20 10:30:46 +0530

    Create designLeadingZeroDetector.v

---
+++ Zero Detector/designLeadingZeroDetector.v
@@ -0,0 +1,19 @@
+module LZD(
+    input [3:0] a,
+    output reg [2:0] x
+);
+
+always @(*) begin
+    if (a[3])
+        x = 3'd0;
+    else if (a[2])
+        x = 3'd1;
+    else if (a[1])
+        x = 3'd2;
+    else if (a[0])
+        x = 3'd3;
+    else
+        x = 3'd4;
+end
+
+endmodule
