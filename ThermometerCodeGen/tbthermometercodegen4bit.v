86a5538c89ce5a122dd03f736968f1ec4b437f7e
avinash1276 <avinashjeevanandam12@gmail.com>
2026-08-21 20:38:25 +0530

    Create tbthermometercodegen4bit.v

---
+++ ThermometerCodeGen/tbthermometercodegen4bit.v
@@ -0,0 +1,16 @@
+module thermometercodegen4bit_tb();
+  reg [3:0]a;
+  wire [14:0]x;
+  thermometercodegen4bit dut(.a(a),.x(x));
+  integer i;
+  initial begin
+    $dumpfile("thermometercodegen4bit.vcd");
+    $dumpvars(0,dut);
+    $monitor("a=%b,x=%b",a,x);
+    for(i=0;i<16;i=i+1) begin
+      a=i;
+      #5;
+    end
+    $finish;
+  end
+endmodule
