71da36a5cbad31c72633854c4b39a73e4318c28b
avinash1276 <avinashjeevanandam12@gmail.com>
2026-08-23 21:09:55 +0530

    Add testbench for minfinder module

---
+++ MINFINDER/tbminfinder.v
@@ -0,0 +1,21 @@
+module minfinder_tb();
+  reg [3:0] a,b,c;
+  wire [3:0] y;
+  minfinder dut(.a(a), .b(b), .c(c), .y(y));
+  initial begin
+    $dumpfile("minfinder.vcd");
+    $dumpvars(0,dut);
+    a = 4'd13;
+    b = 4'd5;
+    c = 4'd15;
+    #5;
+    if (y == a)
+      $display("a=%b, b=%b, c=%b, y=%b -> a is minimum", a,b,c,y);
+    else if (y == b)
+      $display("a=%b, b=%b, c=%b, y=%b -> b is minimum", a,b,c,y);
+    else
+      $display("a=%b, b=%b, c=%b, y=%b -> c is minimum", a,b,c,y);
+    $finish;
+  end
+
+endmodule
