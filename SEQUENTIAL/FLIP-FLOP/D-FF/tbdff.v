ceff083f8425b13d8e5fe4cb2ea8d5e22e23336c
avinash1276 <avinashjeevanandam12@gmail.com>
2026-09-02 11:38:51 +0530

    Create tbdff.v

---
+++ SEQUENTIAL/FLIP-FLOP/D-FF/tbdff.v
@@ -0,0 +1,24 @@
+module dff_tb;
+reg d, clk;
+wire q;
+dff dut (
+    .d(d),
+    .clk(clk),
+    .q(q)
+);
+initial begin
+    $dumpfile("dff.vcd");
+    $dumpvars(0, dff_tb);
+     $monitor("time=%0t, clk=%b, d=%b, q=%b", 
+             $time, clk, d, q);
+    clk = 0;
+    d = 0;
+    #5  d = 1;
+    #5  d = 0;
+    #5  d = 1;
+    #5  d = 0;
+    #5  $finish;
+end
+always #5 clk = ~clk;
+endmodule
+
