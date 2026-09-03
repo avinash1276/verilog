82e4416fb72d50a9c26132f80eecc2cbf09d6a3f
avinash1276 <avinashjeevanandam12@gmail.com>
2026-09-03 15:03:27 +0530

    Create designSRlatch.v

---
+++ SR-LATCH/designSRlatch.v
@@ -0,0 +1,14 @@
+module SRlatch(
+    input s,r,
+    output reg q
+);
+initial q = 0;
+always @(*) begin
+    case({s,r})
+    2'b00: q <= q;
+    2'b10: q <= 1;
+    2'b01: q <= 0;
+    2'b11: q <= 1'bx;
+    endcase
+end
+endmodule
