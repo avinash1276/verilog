9ed4e561bcb11f73fe407743356dec8efcf34491
avinash1276 <avinashjeevanandam12@gmail.com>
2026-09-03 14:56:17 +0530

    Create designJKFF.v

---
+++ JK-FLIPFLOP/designJKFF.v
@@ -0,0 +1,14 @@
+module JKFF(
+    input j,k,clk,
+    output reg q
+);
+initial q = 0;
+always @(posedge clk) begin
+    case({j,k})
+    2'b00: q <= q;
+    2'b10: q <= 1;
+    2'b01: q <= 0;
+    2'b11: q <= ~q;
+    endcase
+end
+endmodule
