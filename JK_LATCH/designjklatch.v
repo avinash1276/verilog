65cceca9fcb35d2f69c3d177d3bc47b5fdafc6f6
avinash1276 <avinashjeevanandam12@gmail.com>
2026-09-11 11:52:18 +0530

    Create designjklatch.v

---
+++ JK_LATCH/designjklatch.v
@@ -0,0 +1,18 @@
+module jklatch(
+    input en,j,k,
+    output reg q
+);
+initial q = 0;
+always@(*) begin
+    if (en)begin
+        case({j,k})
+            2'b00: q=q;
+            2'b01: q=0;
+            2'b10: q=1;
+            2'b11: q=~q;
+        endcase
+    end
+    else
+        q=q;
+end
+endmodule
