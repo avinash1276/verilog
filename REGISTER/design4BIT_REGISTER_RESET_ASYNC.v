dc886ffb0d1ad1d226fd7cd7282fa8d96624bbc2
avinash1276 <avinashjeevanandam12@gmail.com>
2026-09-17 21:39:22 +0530

    Create design4BIT_REGISTER_RESET_ASYNC.v

---
+++ REGISTER/design4BIT_REGISTER_RESET_ASYNC.v
@@ -0,0 +1,23 @@
+module dff(
+    input d,clk,reset,
+    output reg q
+);
+initial q = 0;
+always @(posedge clk or posedge reset) begin
+if(reset)
+    q <= 1'b0;
+else 
+    q <= d;
+end
+endmodule
+module register4dff_resetasync(
+    input [3:0]d,
+    input reset,
+    input clk,
+    output [3:0]q
+);
+dff d1(.d(d[3]),.reset(reset),.clk(clk),.q(q[3]));
+dff d2(.d(d[2]),.reset(reset),.clk(clk),.q(q[2]));
+dff d3(.d(d[1]),.reset(reset),.clk(clk),.q(q[1]));
+dff d4(.d(d[0]),.reset(reset),.clk(clk),.q(q[0]));
+endmodule
