8b1f47aae38eafc9bc52c6e9441b49bef64b5b8c
avinash1276 <avinashjeevanandam12@gmail.com>
2026-09-24 15:32:44 +0530

    Create tbUPCOUNTER_4BIT.v

---
+++ COUNTER_SEQUENTIAL/tbUPCOUNTER_4BIT.v
@@ -0,0 +1,13 @@
+module upcounter4_tb();
+reg clk;
+wire [3:0]q;
+upcounter4 dut(.clk(clk),.q(q));
+always #1 clk = ~clk;
+initial begin
+    $dumpfile("upcounter4.vcd");
+    $dumpvars(0,dut);
+    $monitor("time=%0t clk=%b q=%b",$time,clk,q);
+    clk=0;
+    #64 $finish;
+end
+endmodule
